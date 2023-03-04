668774cf0c1baf60ee36346b0990ea88790f0722
Myktybek <admin@Admins-MacBook-Pro.local>
2023-03-04 10:22:57 +0600

    todo-and-request

---
+++ src/store/store.js
@@ -0,0 +1,12 @@
+import { configureStore } from '@reduxjs/toolkit'
+import postSlice from '../features/post/postSlice'
+import todoSlice from '../features/todo/todoSlice'
+import userSlice from '../features/user/userSlice'
+
+export const store = configureStore({
+    reducer: {
+        user: userSlice,
+        todo: todoSlice,
+        post: postSlice,
+    },
+})
